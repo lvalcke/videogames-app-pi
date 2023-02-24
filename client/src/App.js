f5f2abd89cf43a80b3c17b6f7fd24e02becc4be5
lvalcke <lvalcke88@gmail.com>
2023-02-24 04:33:25 -0500

    initial config

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar.jsx'
+import CreateForm from './components/CreateForm/CreateForm.jsx'
+import Detail from './components/Detail/Detail.jsx'
+import Home from './components/Home/Home.jsx'
+import Landing from './components/Landing/Landing.jsx'
 
 
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar/>
+      <Routes>
+        {/* <Route path='/' element={<NavBar/>}/> */}
+        <Route exact path='/' element={<Landing/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/detail' element={<Detail/>}/>
+        <Route path='/createForm' element={<CreateForm/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ client/src/components/CreateForm/CreateForm.jsx
@@ -0,0 +1,9 @@
+const CreateForm = () => {
+    return (
+        <div>
+            CreateForm
+        </div>
+    )
+}
+
+export default CreateForm
